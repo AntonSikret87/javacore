3d473c153af21ea9d9e2f713b4435c618bd28317
Anton Ka <antonkarabeinikov@gmail.com>
2017-04-04 00:55:03 +0300

    HomeWork Lesson8: SubTask '2-a'

---
+++ src/main/java/com/karabeinikovAntonSikret87/app/homework/ArrayPrint.java
@@ -0,0 +1,14 @@
+package com.karabeinikovAntonSikret87.app.homework;
+
+/**
+ * Created by AntonKa on 4/3/2017.
+ */
+public class ArrayPrint {
+    public static int array[] = {50, 99, 2, 55, 60, 16, 158};
+    public static void main(String[] args) {
+
+        Lesson8HomeWork lesson8HomeWork = new Lesson8HomeWork();
+        lesson8HomeWork.bublAscend(array);
+        lesson8HomeWork.bublDescend(array);
+    }
+}
---
+++ src/main/java/com/karabeinikovAntonSikret87/app/homework/Lesson8HomeWork.java
@@ -0,0 +1,45 @@
+package com.karabeinikovAntonSikret87.app.homework;
+
+import java.util.Arrays;
+
+/**
+ * Created by AntonKa on 4/3/2017.
+ */
+public class Lesson8HomeWork {
+
+    public static int i, j, temp;
+
+
+
+    public static void bublAscend(int[] array) {
+        for (i = 0; i < array.length; i++) {
+            for (j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted accending list of integers:");
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void bublDescend(int[] array) {
+
+        for (i = 0; i < array.length; i++) {
+            for (j = 0; j < array.length - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted descending list of integers:");
+        System.out.println(Arrays.toString(array));
+    }
+}
+
+
+
