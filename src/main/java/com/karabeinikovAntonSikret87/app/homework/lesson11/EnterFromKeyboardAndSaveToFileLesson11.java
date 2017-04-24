ca407fee1aabc07304c57e87f5ef8e53127ababa
Anton Ka <antonkarabeinikov@gmail.com>
2017-04-24 09:25:23 +0300

    added verification if file exist or not

--- src/main/java/com/karabeinikovAntonSikret87/app/homework/lesson11/EnterFromKeyboardAndSaveToFileLesson11.java
+++ src/main/java/com/karabeinikovAntonSikret87/app/homework/lesson11/EnterFromKeyboardAndSaveToFileLesson11.java
@@ -10,8 +10,12 @@ import java.util.Scanner;
  */
 public class EnterFromKeyboardAndSaveToFileLesson11 {
     public static String newCreatedFile = "src\\MyFile\\NewFile.txt";
+    public static String myDir = System.getProperty("user.dir");
+    public static File file = new File(myDir, newCreatedFile);
+    public static boolean existFile = file.exists();
 
     public static void enterTextFromKeyboardAndWriteToFIle(Scanner input, BufferedWriter wr) throws IOException {
+
         System.out.print("Please enter text which will be saved in previously created file (type 'exit' to complete): ");
 
         while (true) {
@@ -24,16 +28,22 @@ public class EnterFromKeyboardAndSaveToFileLesson11 {
         }
         wr.close();
         input.close();
+
     }
 
+
     public static void createFile(Scanner input) throws IOException {
-        String strInput;
-        do {
-            System.out.print("Please enter correct name to create file (File Name should be = 'NewFile.txt') : ");
-            strInput = input.next();
-            File file = new File(newCreatedFile);
-            file.createNewFile();
+        if (!existFile) {
+            String strInput;
+            do {
+                System.out.print("Please enter correct name to create file (File Name should be = 'NewFile.txt') : ");
+                strInput = input.next();
+                File file = new File(newCreatedFile);
+                file.createNewFile();
+            }
+            while (strInput.contains("NewFile.txt") == false);
+        } else {
+            System.out.print("");
         }
-        while (strInput.contains("NewFile.txt") == false);
     }
 }
